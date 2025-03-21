0f1b99a2e86dc1321871455226bd51cc7eef753a
Electrifier <samchijioke16@gmail.com>
2025-03-21 08:08:37 +0100

    feat: Add i18n property to 'ExtendedContext'

--- src/interfaces/context.ts
+++ src/interfaces/context.ts
@@ -1,5 +1,6 @@
+import { I18nContext } from "@grammyjs/i18n";
 import { Context } from "telegraf";
 
-export interface ExtendedContext extends Context {
-    
+export interface ExtendedContext extends Context  {
+    i18n: I18nContext
 }
