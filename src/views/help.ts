8409a2e27c2425b423659146fe3a413f7ee23610
Electrifier <samchijioke16@gmail.com>
2025-03-21 13:52:30 +0100

    feat: Add internationalization support to `HelpController` response  template

--- src/views/help.ts
+++ src/views/help.ts
@@ -1,10 +1,12 @@
+import { I18nContext } from "@grammyjs/i18n";
 import { renderFile } from "ejs";
 import { resolve } from "path";
 
 export class HelpView {
-    static getHelpHtml(name: string) {
+    static getHelpHtml(name: string, i18n: I18nContext) {
         return renderFile(resolve(__dirname, "templates/help.ejs"), {
-            name
+            name,
+            i18n
         });
     }
 }
