662c1e99b580f1d1f9a99cc9c6482b7e14b60fe8
Electrifier <samchijioke16@gmail.com>
2025-03-24 01:44:16 +0100

    fix: Add missing 'wizard' property to ExtendedContext type definition

--- src/interfaces/context.ts
+++ src/interfaces/context.ts
@@ -2,10 +2,12 @@ import { I18nContext } from "@grammyjs/i18n";
 import { User } from "@prisma/client";
 import { Context, Scenes } from "telegraf";
 import { CopperXSession } from "./copperx.interface";
+import { WizardSessionData } from "telegraf/typings/scenes";
 
 export interface ExtendedContext extends Context  {
     copperXSession?: CopperXSession,
     i18n: I18nContext,
-    scene: Scenes.SceneContextScene<ExtendedContext>,
-    user: User
+    scene: Scenes.SceneContextScene<ExtendedContext, WizardSessionData>,
+    user: User,
+    wizard: Scenes.WizardContextWizard<ExtendedContext>
 }
