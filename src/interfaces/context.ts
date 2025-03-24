4a8943f2ad2cb921a328c4f277acfd596ba05da9
Electrifier <samchijioke16@gmail.com>
2025-03-24 16:26:04 +0100

    feat: Extend the 'wizard.state' session data

--- src/interfaces/context.ts
+++ src/interfaces/context.ts
@@ -2,12 +2,21 @@ import { I18nContext } from "@grammyjs/i18n";
 import { User } from "@prisma/client";
 import { Context, Scenes } from "telegraf";
 import { CopperXSession } from "./copperx.interface";
-import { WizardSessionData } from "telegraf/typings/scenes";
+
+interface WizardSessionData extends Scenes.WizardSessionData {
+    userOtp: {
+        email?: string;
+        sid?: string;
+    }
+
+}
 
 export interface ExtendedContext extends Context  {
     copperXSession: CopperXSession,
     i18n: I18nContext,
     scene: Scenes.SceneContextScene<ExtendedContext, WizardSessionData>,
     user: User,
-    wizard: Scenes.WizardContextWizard<ExtendedContext>
+    wizard: Scenes.WizardContextWizard<ExtendedContext> & {
+        state: WizardSessionData
+    }
 }
