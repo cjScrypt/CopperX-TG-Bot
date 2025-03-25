ecf2ae14b8d048f39a6702afd199835deb96e7c5
Electrifier <samchijioke16@gmail.com>
2025-03-25 18:15:34 +0100

    fix: Remove double initialization of session middleware

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,5 @@
 import { Agent } from "https";
-import { session, Telegraf } from "telegraf";
+import { Telegraf } from "telegraf";
 
 import { TG_TOKEN } from "./config";
 import { ExtendedContext } from "./interfaces";
@@ -26,7 +26,6 @@ export const setupBot = () => {
     bot.use(UserMiddleware.addUserToContext);
     bot.use(GlobalMiddleware.addCopperXTokenToContext);
 
-    bot.use(session());
     bot.use(mainStage.middleware());
 
     bot.start(
