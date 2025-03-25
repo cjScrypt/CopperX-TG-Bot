c6637934020dafea84028058b56418c2791b6097
Electrifier <samchijioke16@gmail.com>
2025-03-25 01:58:59 +0100

    feat: Add SceneSession type to context and move CopperXSession to context.session

--- src/interfaces/context.ts
+++ src/interfaces/context.ts
@@ -2,16 +2,10 @@ import { I18nContext } from "@grammyjs/i18n";
 import { User } from "@prisma/client";
 import { Context, Scenes } from "telegraf";
 import { CopperXSession } from "./copperx.interface";
-
-interface WizardSessionData extends Scenes.WizardSessionData {
-    userOtp: {
-        email: string;
-        sid: string;
-    }
-
-}
+import { SceneSession, WizardSessionData } from "./session.interface";
 
 export interface ExtendedContext extends Context  {
+    session: SceneSession,
     copperXSession: CopperXSession,
     i18n: I18nContext,
     scene: Scenes.SceneContextScene<ExtendedContext, WizardSessionData>,
